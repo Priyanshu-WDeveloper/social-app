295f72efe4c8aee45539dce1186adcf8d7b37f0f
Priyanshu Kumar <upiyush005@gmail.com>
2026-05-09 17:39:08 +0530

    some minor bugs

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -107,7 +107,7 @@ export const OAuth = async (req, res) => {
     res.cookie('token', jwtToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
